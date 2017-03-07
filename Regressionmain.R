235ccd66c990dcb06a1d098732d97364eaa7720b
varun kochar <kochar96@gmail.com>
2017-03-07 17:41:50 +0530

    added comments

--- Regressionmain.R
+++ Regressionmain.R
@@ -1,16 +1,19 @@
-train_orig <- read.csv("competition_second_train.csv", header = FALSE)
 
-test_orig <- read.csv("competition_second_test.csv", header = FALSE)
-
+library(rpart)
+library(rpart.plot)
 library(caret)
+library(e1071)
+library(caret)
+library(Rcpp)
+library(mice)
+
+train_orig <- read.csv("competition_second_train.csv", header = FALSE)
+test_orig <- read.csv("competition_second_test.csv", header = FALSE)
 
 set.seed(123)
 newf <- subset(train, select = c(V24,V25,V27,V28,V37,V50,V54,V59,V60,V68))
 
-library(Rcpp)
-library(mice)
 set.seed(125)
-
 imputed_newf = complete(mice(full[var]))
 
 full$V39[is.na(full$V39)] = "SBrkr"
@@ -21,11 +24,7 @@ pred <- predict(lin_reg, newdata = test)
 new <- subset(train, select = -c(V20,V21,V22,V14,V30,V35,V43,V36,V37,V38,V39,V55,V71,V72,V73))
 new <- subset(new, select = -c(V1))
 
-library(rpart)
-library(rpart.plot)
-library(caret)
-library(e1071)
-
+#Using Decision Tree With Cross Validation
 fitControl = trainControl(method = "CV", number = 10)
 CartGrid = expand.grid(.cp = (1:50)*0.01)
 
